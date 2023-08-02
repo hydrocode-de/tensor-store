8e047cd47732fc7c315349f00965ef67d3d3548d
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-08-02 11:22:11 +0200

    added public flag for datasets

--- tensorage/db_init.py
+++ tensorage/db_init.py
@@ -8,6 +8,7 @@ public.datasets (
     shape int[] not null,
     created_at timestamp with time zone null default now(),
     user_id uuid null,
+    is_public boolean not null default false,
     constraint datasets_pkey primary key (id),
     constraint datasets_key_user_id_key unique (key, user_id),
     constraint datasets_user_id_fkey foreign key (user_id) references users (id) on delete set null
@@ -17,9 +18,9 @@ public.datasets (
 ALTER TABLE public.datasets ENABLE ROW LEVEL SECURITY;
 CREATE POLICY "Allow all actions to the record owner" ON "public"."datasets"
 AS PERMISSIVE FOR ALL
-TO public
-USING (auth.uid() = user_id)
-WITH CHECK (auth.uid() = user_id);
+TO authenticated
+USING (is_public OR (auth.uid() = user_id))
+WITH CHECK (is_public OR (auth.uid() = user_id));
 
 
 -- tensor_float4 table
@@ -29,6 +30,7 @@ public.tensors_float4 (
     index bigint not null,
     tensor float4[] not null,
     user_id uuid not null,
+    is_public boolean not null default false,
     constraint tensors_float4_pkey primary key (data_id, index, user_id),
     constraint tensors_float4_data_id_fkey foreign key (data_id) references datasets (id) on delete cascade,
     constraint tensors_float4_user_id_fkey foreign key (user_id) references users (id) on delete set null
@@ -38,9 +40,9 @@ public.tensors_float4 (
 ALTER TABLE public.tensors_float4 ENABLE ROW LEVEL SECURITY;
 CREATE POLICY "Allow all actions to the record owner" ON "public"."tensors_float4"
 AS PERMISSIVE FOR ALL
-TO public
-USING (auth.uid() = user_id)
-WITH CHECK (auth.uid() = user_id);
+TO authenticated
+USING (is_public OR (auth.uid() = user_id))
+WITH CHECK (is_public OR (auth.uid() = user_id));
 
 -- create the slicing database function
 CREATE OR REPLACE FUNCTION public.tensor_float4_slice(name character varying, index_low integer, index_up integer, slice_low integer[], slice_up integer[])
