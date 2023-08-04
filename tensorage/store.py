7cb401c82f5d32b1a96a3621ae77c723bf398339
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-08-04 12:09:48 +0200

    delegate slicing to a new object

--- tensorage/store.py
+++ tensorage/store.py
@@ -1,10 +1,12 @@
-from typing import TYPE_CHECKING, Tuple, Union, List
+from typing import TYPE_CHECKING, Tuple, Union, List, Optional, Any
 from typing_extensions import Literal
 from dataclasses import dataclass, field
 import warnings
 
 from tqdm import tqdm
 import numpy as np
+
+from tensorage.types import Dataset
 
 if TYPE_CHECKING:
     from tensorage.session import BackendSession
@@ -42,61 +44,55 @@ class TensorStore(object):
         else:
             raise ValueError(f"Unknown engine '{self.engine}'.")
 
-    def __getitem__(self, key: Union[str, Tuple[Union[str, slice, int]]]):
+    def get_select_indices(self, key: Union[str, Tuple[Union[str, slice, int]]]) -> Tuple[str, Tuple[int, int], List[Tuple[int, int]]]:
         # first get key
         if isinstance(key, str):
+            key = (key, )
+            name = key
+        elif isinstance(key[0], str):
+            name = key[0]
+        else:
+            raise KeyError('You need to pass the key as first argument.')
+
+        # use the Slicer
+        name, index, slices = StoreSlicer(self, name)(*key[1:])
+        
+        # return the name, index and slices
+        return name, index, slices
+
+    def __getitem__(self, key: Union[str, Tuple[Union[str, slice, int]]]):
+        # the user has to pass the key
+        if isinstance(key, str):
+            key = (key, )  #make it a tuple
             name = key
         elif isinstance(key[0], str):
             name = key[0]
         else:
             raise KeyError('You need to pass the key as first argument.')
         
-        # load the dataset
-        with self.backend.database() as db:
-            dataset = db.get_dataset(name)
+        # instatiate a Slicer
+        slicer = StoreSlicer(self, name)
 
-        # now we need to figure out, what kind of slice we need to pass
-        if isinstance(key, str):
-            index = [1, dataset.shape[0] + 1]
-            slices = [[1, dataset.shape[i] + 1] for i in range(1, dataset.ndim)]
-        
-        # handle all the tuple cases
+        # for now, we accept only iloc-style indexing
+        # TODO if we use the axes, we need to transform here, before instatiating the StoreSlicer
+        return slicer.__getitem__(key[1:])
+
+    def __getattr__(self, key: str):
+        # getattribute did not return anything, so now check if the key is in the keys
+        if key in self._keys:
+            return StoreSlicer(self, key)
         else:
-            # index
-            if isinstance(key[1], int):
-                index = [key[1] + 1, key[1] + 2]
-            elif isinstance(key[1], slice):
-                index = [key[1].start + 1, key[1].stop + 2]
-            else:
-                raise KeyError('Batch index needs to be passed as int or slice.')
-            
-            # slices
-            if len(key) == 2:
-                slices = [[1, dataset.shape[i] + 1] for i in range(2, dataset.ndim)]
-            else:  # more than 2
-                slices = []
-                for i, arg in enumerate(key[2:]):
-                    if isinstance(arg, int):
-                        slices.append([arg + 1, arg + 1])
-                    elif isinstance(arg, slice):
-                        slices.append([arg.start + 1 if arg.start is not None else 1, arg.stop + 1 if arg.stop is not None else dataset.shape[i + 1] + 1])
-                    else:
-                        raise KeyError('Slice needs to be passed as int or slice.')
-                
-                # check if we have all slices
-                if len(slices) + 1 != dataset.ndim:
-                    for i in range(len(slices) + 1, dataset.ndim):
-                        slices.append([1, dataset.shape[i] + 1])
-        
-        # now, name, index and slices are set
-        with self.backend.database() as db:
-            # load the tensor
-            arr = db.get_tensor(name, index[0], index[1], [s[0] for s in slices], [s[1] for s in slices])
-        
-        # TODO now we can transform to other libaries
-        return arr
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
+
+    def __dir__(self):
+        return super().__dir__() + self._keys
 
     def __setitem__(self, key: str, value: Union[List[list], np.ndarray]):
+        # check if the key is already in the database
+        if key in self.keys():
+            # TODO here we need to update the dataset in the database
+            raise NotImplementedError('Updating datasets is not implemented yet.')
+
         # first make a numpy array from it
         if isinstance(value, list):
             value = np.asarray(value)
@@ -169,3 +165,75 @@ class TensorStore(object):
         self._keys = keys
 
         return keys
+
+
+@dataclass
+class StoreSlicer:
+    _store: TensorStore = field(repr=False)
+    key: str
+    dataset: Optional[Dataset] = field(default=None, repr=False)
+
+    def __post_init__(self):
+        if self.dataset is None:
+            with self._store.backend.database() as db:
+                self.dataset = db.get_dataset(self.key)
+
+    def get_iloc_slices(self, *args: Union[int, Tuple[int], slice]) -> Tuple[str, Tuple[int, int], List[Tuple[int, int]]]:
+        # check the length of the args
+        if len(args) == 0:
+            # use the dataset to load the full sample
+            return (
+                self.key, 
+                [1, self.dataset.shape[0] + 1], 
+                [[1, self.dataset.shape[i] + 1] for i in range(1, self.dataset.ndim)]
+            )
+
+        # slicing is actually needed
+        
+        # get the index
+        if isinstance(args[0], int):
+            index = [args[0] + 1, args[0] + 2]
+        elif isinstance(args[0], slice):
+            index = [args[0].start + 1, args[0].stop + 2]
+        else:
+            raise KeyError('Batch index needs to be passed as int or slice.')
+
+        # get the slices
+        if len(args) == 1:
+            slices = [[1, self.dataset.shape[i] + 1] for i in range(2, self.dataset.ndim)]
+        else:  # 2 or more beyond index
+            slices = []
+            for i, arg in enumerate(args[1:]):
+                if isinstance(arg, int):
+                    slices.append([arg + 1, arg + 1])
+                elif isinstance(arg, slice):
+                    slices.append([arg.start + 1 if arg.start is not None else 1, arg.stop + 1 if arg.stop is not None else self.dataset.shape[i + 1] + 1])
+                else:
+                    raise KeyError('Slice needs to be passed as int or slice.')
+            
+            # maybe the user does not want to slice all dimensions, append the others
+            if len(slices) + 1 != self.dataset.ndim:   # +1 for the index
+                for i in range(len(slices) + 1, self.dataset.ndim):
+                    slices.append([1, self.dataset.shape[i] + 1])
+
+        # finally return the full slice index for the database
+        return (
+            self.key,
+            index,
+            slices
+        )
+    
+    def __getitem__(self, args: Union[int, Tuple[int], slice]) -> np.ndarray:
+        # get the slices
+        _, index, slices = self.get_iloc_slices(*args)
+
+        # load the tensor
+        with self._store.backend.database() as db:
+            # load the tensor
+            arr = db.get_tensor(self.key, index[0], index[1], [s[0] for s in slices], [s[1] for s in slices])
+        
+        # TODO now we can transform to other libaries
+        return arr
+
+    def __call__(self, *args: Union[int, Tuple[int], slice]) -> Tuple[str, Tuple[int, int], List[Tuple[int, int]]]:
+        return self.get_iloc_slices(*args)
