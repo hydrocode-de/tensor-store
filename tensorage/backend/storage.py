d1398436664e9e76ebbc11fe4c5fff211c46687b
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-08-04 09:21:41 +0200

    some more netcdf implementation

--- tensorage/backend/storage.py
+++ tensorage/backend/storage.py
@@ -1,7 +1,9 @@
 from typing import List, Tuple
-import os
+import io
+import json
 
 import numpy as np
+import xarray as xr
 from storage3.utils import StorageException
 
 from tensorage.types import Dataset
@@ -51,22 +53,84 @@ class StorageContext(BaseContext):
         return True
     
     def get_dataset(self, key: str) -> Dataset:
-        return super().get_dataset(key)
-    
+        # setup auth token
+        self.__setup_auth()
+
+        # download the metadata
+        buf = io.BytesIO()
+        try:
+            content = self.backend.client.storage.from_(self.user_id).download(f"{key}/dataset.json")
+            buf.write(content)
+        except StorageException as e:
+            if len(e.args) > 0 and e.args[0]['error'] == 'not_found':
+                raise FileNotFoundError(f"Dataset with key '{key}' not found")
+            else:
+                raise e
+
+        # rewind
+        buf.seek(0)
+        dataset = Dataset(**json.load(buf))
+
+        # restore the original auth token
+        self.__restore_auth()
+
+        return dataset
+
     def get_tensor(self, key: str, index_low: int, index_up: int, slice_low: List[int], slice_up: List[int]) -> np.ndarray:
         return super().get_tensor(key, index_low, index_up, slice_low, slice_up)
     
     def insert_dataset(self, key: str, shape: Tuple[int], dim: int, type: str, is_shared: bool) -> Dataset:
-        return super().insert_dataset(key, shape, dim, type, is_shared)
+        # setup auth token
+        self.__setup_auth()
+
+        # create the dataset metadata as a json
+        metadata = json.dumps(dict(id=key, key=key, shape=shape, ndim=dim, type=type, is_shared=is_shared))
+
+        # upload the metadata
+        res = self.backend.client.storage.from_(self.user_id).upload(f"{key}/dataset.json", metadata.encode('utf-8'))
+
+        # restore the original auth token
+        self.__restore_auth()
+
+        return res.json()
     
     def insert_tensor(self, data_id: int, data: List[np.ndarray], offset: int = 0) -> bool:
-        return super().insert_tensor(data_id, data, offset)
-    
+        # setup auth token
+        self.__setup_auth()
+
+        # upload each netcdf chunk
+        for i, arr in enumerate(data):
+            # build the netcdf
+            netcdf = xr.Dataset({data_id: xr.DataArray(arr, dims=[f"dim_{i + 2}" for i in range(arr.ndim)])})
+            
+            # buffer and get the bytes
+            buf = io.BytesIO()
+            netcdf.to_netcdf(buf)
+            buf.seek(0)
+            b_netcdf = buf.getvalue()
+
+            # upload the netcdf
+            self.backend.client.storage.from_(self.user_id).upload(f"{data_id}/index_{int(i + 1 + offset)}.nc", b_netcdf)
+
+        # restore the original auth token
+        self.__restore_auth()
+
+        return True
+
     def append_tensor(self, data_id: int, data: List[np.ndarray]) -> bool:
         return super().append_tensor(data_id, data)
     
     def remove_dataset(self, key: str) -> bool:
-        return super().remove_dataset(key)
+        # setup auth token
+        self.__setup_auth()
+
+        # remove the dataset
+        self.backend.client.storage.from_(self.user_id).remove(f"{key}")
+
+        # restore the original auth token
+        self.__restore_auth()
+        
+        return True
     
     def list_dataset_keys(self) -> List[str]:
         return super().list_dataset_keys()
