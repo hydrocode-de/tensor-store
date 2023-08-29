8c1f46f53b45703ffe06bab2a7bb8b2a80ab9427
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-08-29 11:27:41 +0200

    add the multi-batch tensor insert

--- tests/test_store.py
+++ tests/test_store.py
@@ -47,6 +47,35 @@ class TestTensorStore(unittest.TestCase):
         for expected, actual in zip(data, backend.database.return_value.__enter__.return_value.insert_tensor.call_args[0][1]):
             np.testing.assert_array_almost_equal(expected, actual)
     
+    def test_create_tensor_multiple_batches(self):
+        """
+        Mock the backend of the DatabaseSession and assert that the correct functions
+        have been called.
+        """
+        # create a mock backend
+        backend = MagicMock()
+
+        # create the store
+        store = TensorStore(backend)
+
+        # create the dataset
+        data = np.random.random((30, 10, 10))
+        dataset = Dataset(14, 'test2', data.shape, data.ndim, 'float32', False)
+        backend.database.return_value.__enter__.return_value.insert_dataset.return_value = dataset
+
+        # set a smaller chunk size to simulate multiple batches
+        store.chunk_size = data.shape[1] * data.shape[2] * 2  # data.shape[0] // 2 is batch size
+        
+        # create the tensor
+        store['test2'] = data
+
+        # assert that the dataset has been created
+        backend.database.return_value.__enter__.return_value.insert_dataset.assert_called_once_with('test2', data.shape, data.ndim)
+
+        # assert that the tensor has been created
+        expected_batch_size = int(np.ceil(data.shape[0] / 2))
+        assert backend.database.return_value.__enter__.return_value.insert_tensor.call_count == expected_batch_size
+
 
 if __name__ == '__main__':
     unittest.main()
