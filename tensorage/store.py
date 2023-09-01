46091d64a9d0930b15f900952b1c7261df86f4b5
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-09-01 11:06:07 +0200

    fix empty slice error

--- tensorage/store.py
+++ tensorage/store.py
@@ -333,7 +333,7 @@ class StoreSlicer:
         if isinstance(args[0], int):
             index = [args[0] + 1, args[0] + 2]
         elif isinstance(args[0], slice):
-            index = [args[0].start + 1, args[0].stop + 2]
+            index = [args[0].start + 1 if args[0].start is not None else 1, args[0].stop + 2 if args[0].stop is not None else self.dataset.shape[0] + 1]
         else:
             raise KeyError('Batch index needs to be passed as int or slice.')
 
